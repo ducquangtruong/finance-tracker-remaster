4710bc278d05a92c450b2c1d59480a18555d476d
Duc-Truong <ducqt00@gmail.com>
2025-09-04 23:12:10 +0700

    Shadcn components

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,17 +2,17 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { resolve } from 'node:path';
+import { resolve } from "node:path";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-//   test: {
-//     globals: true,
-//     environment: 'jsdom',
-//   },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
+	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+	//   test: {
+	//     globals: true,
+	//     environment: 'jsdom',
+	//   },
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
+	},
 });
